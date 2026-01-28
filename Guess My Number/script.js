fcd972d64ee463bc3ca23b750e3806674572bbfd
nadilHesara <nadilhesara@gmail.com>
2026-01-28 11:28:20 +0530

    click event practice

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,3 +1,22 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.score').textContent = '33';
+
+document.querySelector('.guess').value = 222;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+});
